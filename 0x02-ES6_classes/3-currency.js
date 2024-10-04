9c03a6bd80ed11293d277338e31c0b47be5b07dc
damaris-lab <damarisnyawiragacheru@gmil.com>
2024-10-04 01:19:02 +0000

    3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,32 +1,32 @@
 export default class Currency {
-	constructor(code, name) {
-		this._code = code;
-		this._name = name;
-	}
+  constructor(code, name) {
+    // Store attributes with underscore prefix
+    this._code = code;
+    this._name = name;
+  }
 
-	//getter for the code
-	get code() {
-		return this._code;
-	}
+  // Getter for code
+  get code() {
+    return this._code;
+  }
 
-	//getter for the name
-	get name() {
-		return this._name;
-	}
-	
-	//setter for the code
-	set code(code_) {
-		this._name = name_;
-	}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-	//setter for the name
-	set name(name_) {
-		this._name = name_;
+  // Setter for code
+  set code(code_) {
+    this._code = code_;
+  }
 
-	}
+  // Setter for name
+  set name(name_) {
+    this._name = name_;
+  }
 
-	//method that returns attrbutes in name and code
-	displayFullCurrency() {
-		return '${this._name} (${this._code})';
-	}
+  // Method that returns attributes in the following format name (code).
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
