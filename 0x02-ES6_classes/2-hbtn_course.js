99c9b3d4f6e203d18ced213408eb89d5080a72d7
damaris-lab <damarisnyawiragacheru@gmil.com>
2024-10-04 01:16:33 +0000

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,44 +1,40 @@
-export default class Holberton {
-	constructor(name, length, students) {
-		if (object.getPrototypeOf(name) !== string.prototype) throw TypeError('name must be a string');
-		if (object.getPrototypeOf(length) !== number.prototype) throw TypeError('length must be a number');
-		if (object.getPrototypeOf(students) !== Array.prototype) throw TypeError('student must be an array');
-		this._name = name;
-		this._length = length;
-		this._students = students;
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (Object.getPrototypeOf(name) !== String.prototype) throw TypeError('name must be a string');
+    if (Object.getPrototypeOf(length) !== Number.prototype) throw TypeError('length must be a number');
+    if (Object.getPrototypeOf(students) !== Array.prototype) throw TypeError('students must be an array');
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-	}
+  get name() {
+    return this._name;
+  }
 
-	get name() {
-		return this._name;
-	}
+  get length() {
+    return this._length;
+  }
 
-	get length() {
-		return this._length;
-	}
+  get students() {
+    return this._students;
+  }
 
-	get students() {
-		return this._students;
-	}
+  set name(name_) {
+    if (Object.getPrototypeOf(name_) !== String.prototype) throw TypeError('name must be a string');
+    this._name = name_;
+  }
 
-	set name() {
-		if (object.getPrototypeOf(name_) !== string.prototype) throw TpeError('name must be a string');
-		this._name = name_;
-	}
+  set length(length_) {
+    if (Object.getPrototypeOf(length_) !== Number.prototype) throw TypeError('length must be a number');
+    this._length = length_;
+  }
 
-	set length() {
-		if (object.getPrototypeOf(length_) !== number.prototype) throw TypeError('length must be a number');
-		this._length = length_;
-	}
-
-	set students() {
-		if (object.getPrototypeOf(students) !== Array.prototype) throw TypeError('student must be an Array'0);
-		if (const i of students_) {
-			if (object.getPrototypeOf(students) !== string.prototype throw TypeError('student must be an Array of strings');
-		}
-		this._students = students_;
-	}
-
-
-	
+  set students(students_) {
+    if (Object.getPrototypeOf(students_) !== Array.prototype) throw TypeError('students must be an array');
+    for (const i of students_) {
+      if (Object.getPrototypeOf(i) !== String.prototype) throw TypeError('students must be an array of strings');
+    }
+    this._students = students_;
+  }
 }
